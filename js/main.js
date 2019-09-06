5c26d8b8ce6973b05475d4b3b20d7556c89cd5c9
e_klimovich <pochta.korobka@gmail.com>
2019-09-06 23:52:57 +0300

    #

--- js/main.js
+++ js/main.js
@@ -1,48 +1,45 @@
-// Slider's
-$(document).ready(function(){
-  const slider = $('.slider-comments');
-  const prev = $('.prev');
-  const next = $('.next');
+$(function() {
+  initSlider('.benefit', 768)
+  initSlider('.direction__content', 768)
+  initSlider('.abonements', 768)
+  initSlider('.slider-comments')
+})
+
+function initSlider(sliderSelecter, maxScreenSize) {
+  if (!!maxScreenSize && $(document).width() > maxScreenSize) {
+    return false;
+  }
+
+  const slider = $(sliderSelecter);
+  const totalCounter = slider.children().length;
+  const arrowPrev = `${sliderSelecter.slice(1)}-prev-arrow`;
+  const arrowNext = `${sliderSelecter.slice(1)}-next-arrow`;
+  const counter = `${sliderSelecter.slice(1)}-current-slide`
 
   slider.slick({
-    prevArrow: prev,
-    nextArrow: next,
-  });
+    arrows: false,
+  })
 
-  slider.on('afterChange', function(event, slick, currentSlide){
-    $("#cp").text(currentSlide + 1);
- });
+  $(sliderSelecter).append(`
+    <div class="slider-controls">
+      <button class="slider-controls__arrow ${arrowPrev}"><img src="./img/controllers/arrow-left.svg" /></button>
+      <span class="slider-controls__counter ${counter}"><span>1</span>/${totalCounter}</span>
+      <button class="slider-controls__arrow ${arrowNext}"><img src="./img/controllers/arrow-right.svg" /></button>
+    </div>
+  `)
 
-  const windowWidth = $(window).width();
-  if(windowWidth < 768){
-    //  sliders
-    let sliderBenefits = $('.benefit');
-    let sliderDirection = $('.direction__content');
+  $(document)
+    .on('click', `.${arrowPrev}`, function() {
+      slider.slick('slickPrev');
+    })
+    .on('click', `.${arrowNext}`, function() {
+      slider.slick('slickNext');
+    })
 
-    // controllers
-    let prev = $('.prev');
-    let next = $('.next');
-    
-    //  slick elements
-      sliderBenefits.slick({
-        prevArrow: prev,
-        nextArrow: next,
-      });
-      
-      sliderBenefits.on('afterChange', function(event, slick, currentSlide){
-        $("#benefitsCs").text(currentSlide + 1);
-      });
-
-      sliderDirection.slick({
-        prevArrow: prev,
-        nextArrow: next,
-      });
-
-      sliderDirection.on('afterChange', function(event, slick, currentSlide){
-          $("#directionCs").text(currentSlide + 1);
-      });
-   }
-});
+  slider.on('afterChange', function(event, slick, count) {
+    $(`.${counter}`).find('span').html(count + 1)
+  })
+}
 
 
 
@@ -52,16 +49,16 @@ var map;
 const fitnesCenter = {lat: 55.757902, lng: 37.582900};
 const castomMarker = $('.castom-marker');
 
-  function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: fitnesCenter,
-      zoom: 15
-    });
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: fitnesCenter,
+    zoom: 15
+  });
 
-    var marker = new google.maps.Marker({
-      position: fitnesCenter,
-      map: map,
-      title: "Anna Hals's",
-      // icon: castomMarker,
-    });
-  }
+  var marker = new google.maps.Marker({
+    position: fitnesCenter,
+    map: map,
+    title: "Anna Hals's",
+    // icon: castomMarker,
+  });
+}
